3d9979e8e5132bc31355040e2ce62436d8be7bfb
jnnoah <jnnoah2211@gmail.com>
2023-06-14 17:07:27 -0400

    Added comments

--- Q1.java
+++ Q1.java
@@ -9,24 +9,30 @@ public class Q1 extends Thread {
 
     @Override
     public void run() {
+        // Iterate from 2 to the given number
         for (int i = 2; i <= number; i++) {
             if (isPrime(i)) {
+                // Print the prime number
                 System.out.println(i);
             }
         }
     }
 
     private boolean isPrime(int number) {
+        // Check if the number is less than or equal to 1, it's not prime
         if (number <= 1) {
             return false;
         }
 
+        // Iterate from 2 to the square root of the number
         for (int i = 2; i <= Math.sqrt(number); i++) {
+            // If the number is divisible by any other number, it's not prime
             if (number % i == 0) {
                 return false;
             }
         }
 
+        // If the number is not divisible by any other number, it's prime
         return true;
     }
 
@@ -35,7 +41,9 @@ public class Q1 extends Thread {
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
+        // Create an instance of Q1 thread with the given number
         Q1 primeThread = new Q1(number);
+        // Start the thread, which will execute the run() method
         primeThread.start();
     }
 }
